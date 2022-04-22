cfe48daeb29e1fdd652e47529eca0129d8e897aa
Filipe Querino <flpqrn@gmail.com>
2022-04-22 20:50:25 -0300

    Iniciando o projeto.

---
+++ app.ts
@@ -0,0 +1,4 @@
+let numero: number = 5;
+let titulo: string = 'TESTE';
+
+console.log(`nome: ${titulo}, idade: ${numero}`);
---
+++ dist/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let numero = 5;
+let titulo = 'TESTE';
+console.log(`nome: ${titulo}, idade: ${numero}`);
