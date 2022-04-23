2a815aea9e62060f4caca8126fff4361b6f17e6c
JadsonLOliveira <100449102+JadsonLOliveira@users.noreply.github.com>
2022-04-22 21:27:37 -0300

    Update app.ts
    Calculos e descontos

--- app.ts
+++ app.ts
@@ -51,3 +51,55 @@ function tabelaInss(salario: number): void{
         dadoUsuario.vl_Agregado = 345.92;
     }
 }
+function descontoInss(salario: number) {
+    return ((dadoUsuario.vl_SalarioBruto - dadoUsuario.vl_BaseFaixa) * dadoUsuario.aliquotaBase ) + dadoUsuario.vl_Agregado;
+}
+
+function descontoIr() {
+    
+    if(vl_ComDescontoInss <= 1903.98 ) {
+        dadoUsuario.descontoIr = 0;
+        dadoUsuario.parcelaDedutivelIr = 0;
+    }
+    if(vl_ComDescontoInss >= 1903.99 && vl_ComDescontoInss <= 2826.65 ) {
+        dadoUsuario.descontoIr = 0.075;
+        dadoUsuario.parcelaDedutivelIr = 142.80;
+    }
+    if(vl_ComDescontoInss >= 2826.66 && vl_ComDescontoInss <= 3751.05 ) {
+        dadoUsuario.descontoIr = 0.15;
+        dadoUsuario.parcelaDedutivelIr = 354.80;
+    }
+    if(vl_ComDescontoInss >= 3751.06 && vl_ComDescontoInss <= 4664.68 ) {
+        dadoUsuario.descontoIr = 0.225;
+        dadoUsuario.parcelaDedutivelIr = 636.13;
+    } 
+    if(vl_ComDescontoInss > 4664.68) {
+        dadoUsuario.descontoIr = 0.275;
+        dadoUsuario.parcelaDedutivelIr = 869.36;
+    }
+}
+
+tabelaInss(dadoUsuario.vl_SalarioBruto);
+
+//Calculo para saber o Desconto do Inss
+var vl_DescontoInss = descontoInss(dadoUsuario.vl_SalarioBruto);
+
+dadoUsuario.faixaDescontoInss = ((vl_DescontoInss * 100)/dadoUsuario.vl_SalarioBruto);
+
+let vl_ComDescontoInss = dadoUsuario.vl_SalarioBruto - vl_DescontoInss
+
+descontoIr();
+
+//Calculo pra saber a faixa de desconto Ir
+dadoUsuario.faixaDescontoIr = ((dadoUsuario.vl_DescontadoIr * 100)/dadoUsuario.vl_SalarioBruto);
+
+var vl_PorHoraTrabalhada = dadoUsuario.vl_SalarioBruto / 200 
+
+var vl_HorasExtras = vl_PorHoraTrabalhada * dadoUsuario.vl_TotalDeHorasExtra
+
+//Calculo Salario Liquido
+dadoUsuario.vl_salarioLiquido = dadoUsuario.vl_SalarioBruto - dadoUsuario.descontoIr - vl_DescontoInss + vl_HorasExtras
+
+
+//Calculo para saber o desconto do Ir
+dadoUsuario.vl_DescontadoIr = ((vl_ComDescontoInss * dadoUsuario.descontoIr ) - dadoUsuario.parcelaDedutivelIr )
